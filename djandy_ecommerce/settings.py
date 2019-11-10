e0d66a635ccc72f50e26158baa8e80c71b133591
Ubuntu <ubuntu@ip-172-31-86-138.ec2.internal>
2019-11-10 19:50:08 +0000

    added cart and products apps

--- djandy_ecommerce/settings.py
+++ djandy_ecommerce/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_forms_bootstrap',
     'accounts',
+    'products',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -65,6 +67,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
+                'cart.contexts.cart_contents'
             ],
         },
     },
@@ -132,6 +136,8 @@ STATICFILE_DIRS = [
 
 MESSAGE_STORAGE ="django.contrib.messages.storage.session.SessionStorage"
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
