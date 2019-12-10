009033ee487ba9cbe2498fec9cca31bf49d02ec6
Ubuntu <ubuntu@ip-172-31-86-138.ec2.internal>
2019-12-10 11:08:21 +0000

    added more products and url for different categories

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url, include
-from .views import all_products
+from .views import all_products, earrings, rings, harmony_bells, bracelets, necklaces
 
 urlpatterns = [
-    url(r'^$', all_products, name='products'), 
+    url(r'^$', all_products, name='products'),
+    url(r'^$', earrings, name='earrings'),
+    url(r'^$', rings, name='rings'),
+    url(r'^$', harmony_bells, name='harmony_bells'),
+    url(r'^$', bracelets, name='bracelets'),
+    url(r'^$', necklaces, name='necklaces'),
     ]
--- products/views.py
+++ products/views.py
@@ -5,3 +5,23 @@ from .models import Product
 def all_products(request):
     products = Product.objects.all()
     return render(request, "products.html", {"products": products})
+    
+def earrings(request):
+    earrings = Product.objects.earrings()
+    return render(request, {"products": earrings})
+    
+def rings(request):
+    rings = Product.objects.rings()
+    return render(request, {"products": rings})
+    
+def harmony_bells(request):
+    harmony_bells = Product.objects.harmony_bells()
+    return render(request, {"products": harmony_bells})
+
+def bracelets(request):
+    bracelets = Product.objects.bracelets()
+    return render(request, {"products": bracelets})
+
+def necklaces(request):
+    necklaces = Product.objects.necklaces()
+    return render(request, {"products": necklaces})
