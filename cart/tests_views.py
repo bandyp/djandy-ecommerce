465b4db90982768c18bb121a2007cd9852261774
Ubuntu <ubuntu@ip-172-31-86-138.ec2.internal>
2020-02-28 09:58:27 +0000

    updated test

--- cart/tests_views.py
+++ cart/tests_views.py
@@ -2,7 +2,6 @@ from django.test import TestCase
 from products.models import Product
 from django.contrib.auth.models import User
 from django.urls import reverse
-from accounts.models import Profile
 
 class TestViews(TestCase):
     def test_get_home_page(self):
